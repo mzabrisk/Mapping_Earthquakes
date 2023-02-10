1bb6d69d883d35010764eeb010be897748a916a4
mzabrisk <mattzabriskie@matts-air.lan>
2023-02-09 20:45:00 -0700

    adding layers and legend

---
+++ Earthquakes_past7days/static/js/logicStep4.js
@@ -0,0 +1,119 @@
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  'Streets': streets,
+  'Satellite': satelliteStreets
+};
+
+// Create the earthquake layer
+let earthquakes = new L.layerGroup();
+
+// Define object that contains the overalays
+let overlays = {
+  Earthquakes: earthquakes
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps, overlays).addTo(map);
+
+// Accessing the Toronto neighborhoods GeoJSON URL.
+let earthquakeJSON = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
+// Grabbing our GeoJSON data.
+d3.json(earthquakeJSON).then(function(data) {
+
+// This function returns the style data for each of the earthquakes we plot on the map. 
+// We pass the magnitude of the earthquake into a function to calculate the radius.
+  function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: getColor(feature.properties.mag),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+    };
+  }
+
+  // This function determines the color of the circle based on the magnitude of the earthquake.
+  function getColor(magnitude) {
+    if (magnitude > 5) {
+      return "#ea2c2c";
+    }
+    if (magnitude >4) {
+      return "#ea822c";
+    }
+    if (magnitude > 3) {
+      return "#ee9c00";
+    }
+    if (magnitude > 2) {
+      return "#eecc00";
+    }
+    if (magnitude > 1) {
+      return "#d4ee00";
+    }
+    return "#98ee00";
+  }
+
+
+// This function determines the radius of the earthquake marker based on its magnitude.
+// Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+  function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+    return magnitude * 4;
+  }
+  
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data, {
+
+// turn each feature in circleMarker
+pointToLayer: function(feature, latlng) {
+          console.log(data);
+          return L.circleMarker(latlng);
+        },
+        // set style for each circle marker
+        style: styleInfo  ,
+        // create popup for each cirlce makerer
+        onEachFeature: function(feature, layer) {
+          layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place)
+        }
+    }).addTo(earthquakes);
+
+    //add earthquake layer to the map
+    earthquakes.addTo(map);
+});
+
+
+// static tiles API
+// https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/1/1/0?access_token=pk.eyJ1IjoibXphYnJpc2siLCJhIjoiY2xkcnQ1cDRuMWdnNTN3cHdzd2x6bDYwaCJ9.OvQj-QtFcWz_fZOLcpJgyA
+
+// alternate ids:
+// mapbox/streets-v11
+// mapbox/outdoors-v11
+// mapbox/light-v10
+// mapbox/dark-v10
+// mapbox/satellite-v9
+// mapbox/satellite-streets-v11
